543690cccebaacd18d5ae428ccb90bd63f77fb57
jagathi <jagathi.anand@gmail.com>
2016-01-29 20:17:45 +0530

    Update count2.c

--- count2.c
+++ count2.c
@@ -2,18 +2,18 @@
 #include<conio.h>
 void main()
 {
-     int num,j=0;
-     printf("input:");
-     scanf("%d",&num);
-     if(num==0)
-     {
-          j=1;
-     }
-     while(num!=0)
-     {
-	              num=num/2;
-	              j++;
-	   }
-     printf("no.of bits:%d\n",j);
-     getch();
+      int num,j=0;
+      printf("input:");
+      scanf("%d",&num);
+      if(num==0)
+      {
+            j=1;
+      }
+      while(num!=0)
+      {
+	    num=num/2;
+	    j++;
+      }
+      printf("no.of bits:%d\n",j);
+      getch();
 }
