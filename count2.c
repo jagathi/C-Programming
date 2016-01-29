d2ac680716d9b0b65c9c85319d4fd8e4886cc206
jagathi <jagathi.anand@gmail.com>
2016-01-29 20:12:08 +0530

    Create count2.c

---
+++ count2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int num,j=0;
+     printf("input:");
+     scanf("%d",&num);
+     if(num==0)
+     {
+          j=1;
+     }
+     while(num!=0)
+     {
+	              num=num/2;
+	              j++;
+	   }
+     printf("no.of bits:%d\n",j);
+     getch();
+}
